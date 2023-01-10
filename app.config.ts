2d9966b5cd5a5a848465ec8c945fd987dc93c16e
thuongnv <thuongnv@ftech.ai>
2023-01-10 11:21:00 +0700

    update

--- app.config.ts
+++ app.config.ts
@@ -1,48 +1,49 @@
 export default defineAppConfig({
   github: {
-    owner: 'nuxt',
-    repo: 'content',
-    branch: 'main'
+    owner: "nuxt",
+    repo: "content",
+    branch: "main",
   },
   docus: {
-    title: 'Nuxt Content',
-    description: 'Write pages in markdown, use Vue components and enjoy the power of Nuxt with a blazing fast developer experience.',
-    layout: 'docs',
-    image: 'https://content.nuxtjs.org/preview.png',
-    url: 'https://content.nuxtjs.org',
+    title: "Nuxt Content",
+    description:
+      "Write pages in markdown, use Vue components and enjoy the power of Nuxt with a blazing fast developer experience.",
+    layout: "docs",
+    image: "https://content.nuxtjs.org/preview.png",
+    url: "https://content.nuxtjs.org",
     socials: {
-      twitter: '@nuxt_js',
-      github: 'nuxt/content'
+      twitter: "@nuxt_js",
+      github: "nuxt/content",
     },
     github: {
-      root: 'docs/content',
+      root: "docs/content",
       edit: true,
-      releases: true
+      releases: true,
     },
     cover: {
-      src: '/cover.jpg',
-      alt: 'Content made easy for Vue developers'
+      src: "/cover.jpg",
+      alt: "Content made easy for Vue developers",
     },
     aside: {
-      level: 1
+      level: 1,
     },
     header: {
-      logo: 'Logo',
-      exclude: ['/v1', '/content-v1', '/fr', '/ja', '/ru']
+      logo: "Logo",
+      exclude: ["/v1", "/content-v1", "/fr", "/ja", "/ru"]
     },
     footer: {
       iconLinks: [
         {
-          label: 'NuxtJS',
-          href: 'https://nuxtjs.org',
-          icon: 'IconNuxt'
+          label: "NuxtJS",
+          href: "https://nuxtjs.org",
+          icon: "IconNuxt",
         },
         {
-          label: 'Vue Telescope',
-          href: 'https://vuetelescope.com',
-          icon: 'IconVueTelescope'
-        }
-      ]
-    }
-  }
-})
+          label: "Vue Telescope",
+          href: "https://vuetelescope.com",
+          icon: "IconVueTelescope",
+        },
+      ],
+    },
+  },
+});
