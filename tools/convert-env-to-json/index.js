28eeddd0fca10db06b8065b5a9201fa13d6bf75d
thuongnv <thuongnv@ftech.ai>
2023-01-12 15:54:57 +0700

    update

--- tools/convert-env-to-json/index.js
+++ tools/convert-env-to-json/index.js
@@ -1,9 +1,33 @@
 var http = require("http");
+const { parse } = require('dotenv');
+const fs = require('fs')
+const YAML = require('json-to-pretty-yaml');
+
+
+const toEnvK8s = (env) => {
+    const p = parse(env);
+    const result = []
+    Object.keys(p).forEach((key)=>{
+       result.push({name:key,value:p[key]})
+    })
+    return YAML.stringify(result)
+};
 
 //create a server object:
 http
   .createServer(function(req, res) {
-    res.write("Hello World!");
+    const envContent = fs.readFileSync('./.env.example')
+    const p = parse(envContent)
+    const yamlK8s = toEnvK8s(envContent)
+    const w = `
+${JSON.stringify(p)}
+
+==========================
+
+${yamlK8s}
+    `
+
+    res.write(w);
     res.end();
   })
   .listen(8080);
